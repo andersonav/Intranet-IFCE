6cf60654929e57cb587158e1eedd3f78d51db466
Anderson Alves <anderson.alves_ce@outlook.com>
2018-08-28 16:35:58 -0300

    Lógica para dropdown right menu realizada. Falta apenas responsividade.

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -4,16 +4,67 @@
  * and open the template in the editor.
  */
 
-
 $(document).ready(function () {
-    $("#imgActiveDropdown").click(function () {
-        $('.ui.dropdown').dropdown();
+    var aux = "";
+    var indiceId = {"system": 0, "notifications": 0, "informationsUser": 0};
+    $('.menu .item').tab();
+    $('.checkbox').checkbox();
+    $('select').dropdown();
+
+    $(".openbtn").on("click", function () {
+        $(".ui.sidebar").toggleClass("very thin icon");
+        $(".asd").toggleClass("marginlefting");
+        $(".sidebar z").toggleClass("displaynone");
+        $(".ui.accordion").toggleClass("displaynone");
+        $(".ui.dropdown.item.iconShort").toggleClass("displayblock");
+        $(".teste").toggleClass("testeMarginLefting");
+        $("#sibebarMaior").toggleClass("sibebarToMenor");
+        $(".logo").find('img').toggle();
+
+    });
+    $(".openDivNone").click(function () {
+        var valorId = $(this).attr("id");
+        $(".divMae").each(function () {
+            $(this).removeClass("displayblock");
+            $(this).addClass("displaynone");
+        });
+        $("#iconsAndInformationsUser .arrow").each(function () {
+            $(this).removeClass("displayblock");
+            $(this).addClass("displaynone");
+        });
+        if (indiceId[valorId] == 0) {
+            $(".divMae#" + valorId).addClass("displayblock");
+            $("#iconsAndInformationsUser .arrow#" + valorId).addClass("displayblock");
+            auxIndiceArray(valorId, 1);
+        } else {
+            $(".divMae#" + valorId).removeClass("displayblock");
+            $("#iconsAndInformationsUser .arrow#" + valorId).removeClass("displayblock");
+            $(".divMae#" + valorId).addClass("displaynone");
+            $("#iconsAndInformationsUser .arrow#" + valorId).addClass("displaynone");
+            auxIndiceArray(valorId, 0);
+        }
     });
 
-    $(".sanduiche").click(function () {
-        $('.menuOpenAndClose').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
-        $('.ui.accordion').accordion();
-        $("body").removeClass("pushable");
+    function auxIndiceArray(valorId, valorInteiro) {
+        if (valorId == "system") {
+            indiceId[valorId] = valorInteiro;
+            indiceId["notifications"] = 0;
+            indiceId["informationsUser"] = 0;
+        } else if (valorId == "notifications") {
+            indiceId[valorId] = valorInteiro;
+            indiceId["system"] = 0;
+            indiceId["informationsUser"] = 0;
+        } else {
+            indiceId[valorId] = valorInteiro;
+            indiceId["system"] = 0;
+            indiceId["notifications"] = 0;
+        }
+    }
 
-    });
+//    $('.ui.accordion').accordion({
+//        selector: {
+//
+//        }
+//    });
+
 });
