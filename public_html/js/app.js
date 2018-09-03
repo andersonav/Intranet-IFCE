f8f823e303ceff6c618dffbbd5b2aa08ab529dce
Anderson Alves <anderson.alves_ce@outlook.com>
2018-09-03 16:07:55 -0300

    Adicionado layout para mobile. Falta configurar pixels dos ícones na resolução específica e accordion para active.

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -13,30 +13,36 @@ $(document).ready(function () {
 
     if ($(window).width() >= 200 && $(window).width() <= 549) {
         $("#sibebarMaior").removeClass("visible");
+        $("#iconSidebarMobile").click(function () {
+            $("#sibebarMaior").addClass("visible");
+            var aux = auxWidthScreen();
+            $("#sibebarMaior").css("width", aux + "%");
+            $("#sibebarMaior").css("display", "block");
+            $(".openbtn").addClass("closeMobile");
+            clickDeviceMobile();
+        });
+    } else {
+        $(".openbtn#btnDevices").click(function () {
+            $(".ui.sidebar").toggleClass("very thin icon");
+            $(".asd").toggleClass("marginlefting");
+            $(".sidebar z").toggleClass("displaynone");
+            $(".ui.accordion").toggleClass("displaynone");
+            $(".ui.dropdown.item.iconShort").toggleClass("displayblock");
+            var aux = auxWidthScreen();
+            $(".teste").toggleClass(aux);
+            $("#sibebarMaior").toggleClass("sibebarToMenor");
+            $(".logo").find('img').toggle();
+
+        });
     }
 
-    $(".openbtn").on("click", function () {
-        $(".ui.sidebar").toggleClass("very thin icon");
-        $(".asd").toggleClass("marginlefting");
-        $(".sidebar z").toggleClass("displaynone");
-        $(".ui.accordion").toggleClass("displaynone");
-        $(".ui.dropdown.item.iconShort").toggleClass("displayblock");
-        var aux = "";
-        if ($(window).width() >= 550 && $(window).width() <= 849) {
-            aux = "marginLeftingResolutionMenor";
-            // is mobile device
-        } else {
-            aux = "testeMarginLefting";
-        }
-        $(".teste").toggleClass(aux);
-        $("#sibebarMaior").toggleClass("sibebarToMenor");
-        $(".logo").find('img').toggle();
+    function clickDeviceMobile() {
+        $(".closeMobile").click(function () {
+            $("#sibebarMaior").removeClass("visible");
+            $(this).removeClass("closeMobile");
+        });
+    }
 
-    });
-
-    $("#iconSidebarMobile").click(function () {
-        $("#sibebarMaior").addClass("visible");
-    });
 
     $(".openDivNone").click(function () {
         var valorId = $(this).attr("id");
@@ -76,6 +82,27 @@ $(document).ready(function () {
             indiceId["notifications"] = 0;
         }
     }
+
+    function auxWidthScreen() {
+        var aux = "";
+        if ($(window).width() >= 550 && $(window).width() <= 849) {
+            aux = "marginLeftingResolutionMenor";
+            // is mobile device
+        } else if ($(window).width() >= 500 && $(window).width() <= 549) {
+            aux = "50";
+        } else if ($(window).width() >= 450 && $(window).width() <= 499) {
+            aux = "55";
+        } else if ($(window).width() >= 350 && $(window).width() <= 449) {
+            aux = "68";
+        } else if ($(window).width() >= 200 && $(window).width() <= 349) {
+            aux = "80";
+        } else {
+            aux = "testeMarginLefting";
+        }
+        return aux;
+    }
+    
+    
 
     $('.ui.accordion').accordion({
         selector: {
